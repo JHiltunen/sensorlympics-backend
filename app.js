3d332fe27a526e60129a2cd7b1fbd943ccec805c
Joonas Hiltunen <joonas.s.jh@gmail.com>
2022-03-09 19:47:12 +0200

    Change data that is emmitted

--- app.js
+++ app.js
@@ -20,8 +20,9 @@ io.on('connection', (socket) => {
         
         count++;
         console.log("DATA", room_data);
-        io.emit('counter', count);   
-        io.emit('xyCoordinates')
+        io.emit('counter', room_data);   
+        //io.emit('data', room_data);
+        //io.emit('xyCoordinates')
         socket.join(`${roomName}`);
         // write new message of new user
         io.to(`${roomName}`).emit('newUserToChatRoom');
