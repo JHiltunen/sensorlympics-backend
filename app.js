616386399c1676094842ae7bd30391f76bd5d0b8
Joonas Hiltunen <joonas.s.jh@gmail.com>
2022-03-09 15:25:47 +0200

    Base project

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express'); //requires express module
+const socket = require('socket.io'); //requires socket.io nodule
+const ts = require('fs');
+const app = express();
+var PORT = process.env. PORT || 3000; var PORT;
+const server = app.listen(PORT); //tells to host server on localhost:3000
+//Playing variables:
+app.use(express.static('public')); //show static files in 'pub lic' directory
+console. log('Server is running');
+const io = socket(server);
+var count = 0;
+//PS Socket.io Connection-
+io.on('connection', (socket) => {
+    socket.on('create', function(room) {
+        const room_data = JSON.parse(room)
+        const roomName = room_data.roomName;
+    
+        count++;
+        io.emit('counter', count);   
+        socket.join(`${roomName}`);
+        // write new message of new user
+        io.to(`${roomName}`).emit('newUserToChatRoom');
+    });
+    console.log("New socket connection:" + socket.id);
+});
+
+io.on('newMessage',function(data) {
+    console.log('newMessage triggered')
+
+    const messageData = JSON.parse(data)
+    const messageContent = messageData.content
+    const roomName = messageData.roomName
+
+    console.log(`[Room Number ${roomName}] : ${messageContent}`)
+    // Just pass the data that has been passed from the writer socket
+
+    const chatData = {
+        messageContent : messageContent,
+        roomName : roomName
+    }
+    console.log('JSON', JSON.stringify(chatData));
+    socket.broadcast.to(`${roomName}`).emit('updateChat',JSON.stringify(chatData)) // Need to be parsed into Kotlin object in Kotlin
+})
+
+/*io.sockets.in("room1").emit('counter', () => {
+    count++;
+    io. emit('counter', count);   
+});*/
